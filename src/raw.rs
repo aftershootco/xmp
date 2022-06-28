6edd9b98a99796588d81102366b3c28109293884
Uttarayan Mondal <email@uttarayan.me>
2022-06-28 18:58:29 +0500

    Get xmp data using the safe wrapper

--- src/raw.rs
+++ src/raw.rs
@@ -4,34 +4,8 @@ use std::io::Cursor;
 pub(crate) fn __raw_load_xml(path: impl AsRef<Path>) -> Result<Vec<u8>, XmpError> {
     let mut processor = libraw_r::Processor::default();
     processor.open(path)?;
-    let iparams = processor.iparams();
-
-    let xmp = unsafe {
-        std::slice::from_raw_parts(
-            std::mem::transmute(iparams.xmpdata),
-            iparams.xmplen as usize,
-        )
-    };
+    let xmp = processor.xmpdata()?;
     Ok(xmp.to_vec())
-}
-
-impl UpdateResults {
-    // pub fn update_raw(
-    //     &self,
-    //     path: impl AsRef<Path>,
-    //     options: UpdateOptions,
-    // ) -> Result<(), XmpError> {
-    //     let xml = self
-    //         .update_xml(BufReader::new(std::fs::File::open(&path)?), options)
-    //         // .update_xml(std::fs::read(&path)?.as_slice(), false)
-    //         .unwrap_or_else(|_e| DEFAULT_XML.as_bytes().to_vec());
-
-    //     let mut bfw = BufWriter::new(std::fs::File::create(path)?);
-    //     bfw.write_all(xml.as_slice())?;
-    //     bfw.flush()?;
-
-    //     Ok(())
-    // }
 }
 
 impl OptionalResults {
