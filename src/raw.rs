69afbe55fff004d7fec978df320b4049bf6b5807
Arsh <arshkumar9@live.com>
2022-10-28 15:30:46 +0530

    Added tag to get xml containing rating.

--- src/raw.rs
+++ src/raw.rs
@@ -3,7 +3,21 @@ use std::io::Cursor;
 
 pub(crate) fn __raw_load_xml(path: impl AsRef<Path>) -> Result<Vec<u8>, XmpError> {
     let mut processor = libraw_r::Processor::default();
+    let exif = processor.set_exif_callback(
+        Vec::<u8>::new(),
+        libraw_r::exif::DataStreamType::File,
+        |container, tag, _, _, _, data, _| {
+            if tag ==  0x02bc {
+                container.extend_from_slice(data);
+            }
+            Ok(())
+        },
+    )?;
     processor.open(path)?;
+    let xmp = exif.data(&mut processor)?;
+    if !xmp.is_empty() {
+        return Ok(xmp);
+    }
     let xmp = processor.xmpdata()?;
     Ok(xmp.to_vec())
 }
